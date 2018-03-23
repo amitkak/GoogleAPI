46af5d20e40ff8212ec32bdac6df115823f4783a
EC2 Default User <ec2-user@ip-172-31-20-35.us-west-2.compute.internal>
2018-03-23 06:43:07 +0000

    DB Version for dailyscans on # Please enter the commit message for your changes. Lines starting

---
+++ gssDB.py
@@ -0,0 +1,217 @@
+
+from __future__ import print_function
+import httplib2
+import os
+import time
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+f1 = open("vol.dat","w")
+f2 = open("chg.dat", "w")
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def get_sheetDetails():
+
+    """ Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    """spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+       rangeName = 'Class Data!A2:E'
+    """
+    #spreadsheetId = '1TVremAZCmLjXD-q6FbHxdVR5-xMdt73O4qXMEXodKF8'
+    spreadsheetId = '1SZma3WDG8UrNrvDYT7MErSXlcpLa0VPbWwQlMBkeN6c'
+    return spreadsheetId,service
+
+
+def get_price():
+    
+    spreadsheetId, service = get_sheetDetails()
+    
+    rangeName = 'F62:F62'
+
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        #print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print('%s, %s' % (row[0], row[1]))
+            return row[0]
+def get_hi():
+
+    spreadsheetId, service = get_sheetDetails()
+
+    rangeName = 'F63:F63'
+
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        #print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print('%s, %s' % (row[0], row[1]))
+            #print('%s' % (row[0]))
+            return row[0]
+
+def get_lo():
+
+    spreadsheetId, service = get_sheetDetails()
+
+    rangeName = 'F64:F64'
+
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        #print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print('%s, %s' % (row[0], row[1]))
+            return row[0]
+
+def get_pct():
+
+    spreadsheetId, service = get_sheetDetails()
+
+    rangeName = 'F65:F65'
+
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        #print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print('%s, %s' % (row[0], row[1]))
+            return row[0]
+
+
+def update_cell(symbol):
+
+    
+    spreadsheetId, service = get_sheetDetails()
+    cell = symbol
+    
+    values = [
+        [
+            # Cell values ...
+            cell
+        ],
+        # Additional rows ...
+    ]
+    #values = [ 'QCOM' ]
+
+    body = {
+        'values': values
+    }
+
+    rangeName = 'A76'
+    value_input_option = 'USER_ENTERED'
+    #value_input_option = 'QCOM'
+    result = service.spreadsheets().values().update(spreadsheetId=spreadsheetId,range = rangeName, valueInputOption=value_input_option, body=body).execute()
+    time.sleep(6)
+
+    
+
+
+def main():
+    """Shows basic usage of the Sheets API."""
+
+
+    #symbool_list = ['QCOM','AAPL','BUD','RIG','MSFT','BABA','SU','YUM','GILD','AMGN','BXP','CSCO','TXN','NVDA']
+
+
+    symbool_list = ['TSLA','INTC','BIIB','IBM','AAPL','QCOM','AVGO','CVS','NVDA','FB','EXPE','GS','AMZN','GOOG']
+
+
+    #symbool_list = ['QCOM','AAPL','AVGO','WMT']
+ 
+
+    for symbol in symbool_list:
+        print ("++++++++++++++++++++++++++++++++")
+        print (symbol)
+        update_cell(symbol)
+        price = get_price()
+        hi52 = get_hi()
+        lo52 = get_lo()
+        change= get_pct()
+        eps = 0
+        pe = 0
+        avgvol = 0
+        vol = 0
+
+        print ("++++++++++++++++++++++++++++++++")
+        f1.write("Company\t" + symbol + "\tVOL\t" + str(vol) + "\tAVGVOL\t"   + str(avgvol)  + "\tPCENTCHG\t" + str(change) + "\tEPS\t" + str(eps) + "\tPE\t" + str(pe) +"\n")
+        f2.write("Company\t" + symbol + "\t52 Week Range\t" + str(lo52) + "-" + str(hi52) +  "\tPrice\t" + str(price) + "\tEPS\t" + str(eps)+ "\tPE\t" + str (pe)+"\n")
+
+
+
+if __name__ == '__main__':
+    main()
+
